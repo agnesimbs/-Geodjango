d97b6a7f11773ab3c8003985700e7e68ed9b9974
agnesimbs <agneswambui504@gmail.com>
2018-11-06 12:00:04 +0300

    ...

--- agricom/pages/admin.py
+++ agricom/pages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Movers
+from .models import Movers,Bookings
 # Register your models here.
 
 admin.site.register(Movers)
+admin.site.register(Bookings)
